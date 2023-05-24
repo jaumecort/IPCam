83fb7fc00c78369beb7f953d5d7ac38c732f5577
jaumecort <jaume.cort@gmail.com>
2023-05-24 15:45:26 +0200

    fastcam

---
+++ etc/FastCam/FastCam.py
@@ -0,0 +1,49 @@
+import re
+import sys
+from os import system
+import subprocess
+import OnvifDiscoverer
+
+if __name__ == "__main__":
+
+    port="554"
+    cmdVLC=r'C:\Program Files\VideoLAN\VLC\vlc'
+
+    print("Buscando camaras...")
+
+    bc ="255.255.255.255"
+    dev = OnvifDiscoverer.OnvifDiscovery(bc)
+
+    if not dev:
+        print("No se han encontrado camaras")
+        input("Presiona qualquier tecla para salir")
+        sys.exit()
+
+    for uri in dev: 
+        if 'onvif' in uri: 
+            ip = dev[uri]
+            print("Camara encontrada en IP:" + ip)
+
+    i = input("Credenciales por defecto? (s/n) ")
+
+    while not (i.lower()=='s' or i.lower()=='n'):
+        print(i)
+        i = input("Credenciales por defecto? (s/n)")
+
+    if i.lower() == 's':
+        user="admin"
+        password="L2F63400"
+        url = r'rtsp://'+user+r':'+password+r'@'+ip+r':'+port+r'/cam/realmonitor?channel=1&&subtype=0&&unicast=true&&proto=Onvif'
+          
+    else:
+        url = r'rtsp://'+ip+':'+port+r'/cam/realmonitor?channel=1&&subtype=0&&unicast=true&&proto=Onvif'
+
+    
+    cmd = cmdVLC+" "+url
+    print(cmd)
+    #system(cmd)
+    subprocess.call([cmdVLC, url], shell=False)
+    input()
+
+
+ 
--- etc/FastCam/OnvifDiscoverer.py
+++ etc/FastCam/OnvifDiscoverer.py
@@ -7,12 +7,11 @@ import select
 
 def OnvifDiscovery(broadcastIP):
     #returns:
-    deviceAddrs = []
-    deviceUris = []
+    devices = {}
 
     #constants:
     udpDiscoveryPort = 3702 # IANA port number for web Service Discovery
-    discoveryTime = 3 # 3 seconds
+    discoveryTime = 2 # 3 seconds
 
     # send a UDP WS-Discovery probe to get URI of ONVIF device service. uuid.uuid4() produces randum uuid.
     udpProbe = '<?xml version="1.0" encoding="utf-8"?><Envelope xmlns:dn="http://www.onvif.org/ver10/network/wsdl" xmlns="http://www.w3.org/2003/05/soap-envelope"><Header><wsa:MessageID xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing">' + str(uuid.uuid4()) + '</wsa:MessageID><wsa:To xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing">urn:schemas-xmlsoap-org:ws:2005:04:discovery</wsa:To><wsa:Action xmlns:wsa="http://schemas.xmlsoap.org/ws/2004/08/addressing">http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</wsa:Action></Header><Body><Probe xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns="http://schemas.xmlsoap.org/ws/2005/04/discovery"><Types>dn:NetworkVideoTransmitter</Types><Scopes /></Probe></Body></Envelope>'
@@ -27,14 +26,15 @@ def OnvifDiscovery(broadcastIP):
         if ready[0]:
             data, addr = sock.recvfrom(65536) 
             if data: 
-                deviceAddrs.append(addr[0])
+                #deviceAddrs.append(addr[0])
                 data = str(data)
                 m = re.search(r'XAddrs\s*>(.*?)<', data)
                 deviceUri = m.group(1).strip()
-                deviceUris.append(deviceUri)
+                #deviceUris.append(deviceUri)
+                devices[deviceUri]=addr[0]
         else: break
 
-    return [deviceAddrs, deviceUris]
+    return devices
 
 if __name__=="__main__":
     broadcastIP = input('Broadcast IP? \n\t(r=192.168.88.255) \t(l=147.83.49.255) \t(d=147.83.49.78)\n')
